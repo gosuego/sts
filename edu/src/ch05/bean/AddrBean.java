7f0424cf779ea818109b1ff23a238aaf7bbae08b
gosuego <cankate@hanmail.net>
2020-09-22 14:46:08 +0900

    객체 하나하나가 배열의 값

--- edu/src/ch05/bean/AddrBean.java
+++ edu/src/ch05/bean/AddrBean.java
@@ -1,10 +1,36 @@
 package ch05.bean;
 
 public class AddrBean {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//멤버변수 선언
+	private String username;
+	private String tel;
+	private String email;
+	private String sex;
+	
+	
+	public String getUsername() {
+		return username;
 	}
-
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
 }
